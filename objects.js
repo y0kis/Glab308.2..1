161e90c6065ed852ad893e6e464e092c2cc2b2d2
y0kis <yokish1986@gmail.com>
2024-07-17 23:23:32 -0400

    Glab308.2..1

--- objects.js
+++ objects.js
@@ -1,19 +1,19 @@
-const adventurer = {
+// const adventurer = {
+//     name: "Robin",
+//     health: 10,
+//     inventory: ["sword", "potion", "artifact"]
+//     }
+//     console.log(adventurer.inventory);
+    
+    const adventurer = {
     name: "Robin",
     health: 10,
-    inventory: ["sword", "potion", "artifact"]
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat"
+    }
     }
     console.log(adventurer.inventory);
-    
-    // const adventurer = {
-    // name: "Robin",
-    // health: 10,
-    // inventory: ["sword", "potion", "artifact"],
-    // companion: {
-    //     name: "Leo",
-    //     type: "Cat"
-    // }
-    // }
-    // console.log(adventurer.inventory);
 
 
